d8e9f8628679b85b4143648889e4d89842fdd242
Andy Adams <aadams@jestro.com>
2012-11-15 09:31:33 -0800

    Further hookups of basic options.

--- demo-site.php
+++ demo-site.php
@@ -9,9 +9,9 @@ Author URI: #
 */
 
 function demo_site_plugin_init() {
-	register_setting( 'demo_site_plugin', 'demo_site_plugin_options' );
-	add_settings_section( 'demo_site_plugin_main', 'Main Settings', 'demo_site_plugin_section_text', 'demo_site_plugin' );
-	add_settings_field( 'demo_site_plugin_string', 'Plugin Text Input', 'demo_site_plugin_string_html', 'demo_site_plugin', 'demo_site_plugin_main' );
+	register_setting( 'demo_site_plugin_options', 'demo_site_plugin_options' );
+	add_settings_section( 'demo_site_plugin_main', 'Main Settings', 'demo_site_plugin_section_text', 'demo_site_plugin_options' );
+	add_settings_field( 'demo_site_plugin_string', 'Plugin Text Input', 'demo_site_plugin_string_html', 'demo_site_plugin_options', 'demo_site_plugin_main' );
 }
 add_action( 'admin_init', 'demo_site_plugin_init' );
 
@@ -22,8 +22,9 @@ function demo_site_plugin_section_text() {
 }
 
 function demo_site_plugin_string_html() {
+	$options = get_option( 'demo_site_plugin_options' );
 	?>
-	<h3>hey hey hey</h3>
+	<input id='plugin_text_string' name='demo_site_plugin_options[demo_site_plugin_string]' size='40' type='text' value='<?php echo $options['demo_site_plugin_string']; ?>' />
 	<?php
 }
 
@@ -39,9 +40,9 @@ function demo_site_plugin_options_form() {
 		<?php screen_icon(); ?>
 		<h2><?php _e( 'Demo site plugin options', 'demo_site_plugin' ); ?></h2>
 		<form method="post" action="options.php">
-			<?php settings_fields( 'demo_site_plugin' ); ?>
-			<?php do_settings_sections( 'demo_site_plugin', 'demo_site_plugin_options' ); ?>
-			<input type="submit" class="button-primary" name="set_defaults" value="<?php _e( 'Set defaults', 'demo_site_plugin' ); ?>">
+			<?php settings_fields( 'demo_site_plugin_options' ); ?>
+			<?php do_settings_sections( 'demo_site_plugin_options', 'demo_site_plugin_options' ); ?>
+			<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
 		</form>
 	</div>
 	<?php
